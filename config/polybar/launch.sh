9e2cb56fa95c0e2d92f3419093a94dabe7ebd601
Hiansdt <vnhiansdt@gmail.com>
2025-08-22 15:09:50 -0300

    Polybar updates: Separate bars for different modules

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,5 +3,28 @@ DIR="$HOME/.config/polybar"
 killall -q polybar
 while pgrep -u "$UID" -x polybar >/dev/null; do sleep 0.1; done
 
-polybar -q main -c "$DIR"/config.ini &
-polybar -q secondary -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config-main.ini &
+polybar -q main_2 -c "$DIR"/config-main.ini &
+polybar -q main_3 -c "$DIR"/config-main.ini &
+polybar -q main_4 -c "$DIR"/config-main.ini &
+polybar -q main_5 -c "$DIR"/config-main.ini &
+polybar -q secondary -c "$DIR"/config-secondary.ini &
+polybar -q secondary_2 -c "$DIR"/config-secondary.ini &
+polybar -q secondary_3 -c "$DIR"/config-secondary.ini &
+polybar -q secondary_4 -c "$DIR"/config-secondary.ini &
+polybar -q secondary_5 -c "$DIR"/config-secondary.ini &
+
+
+
+# for terminal testing
+#
+# polybar -q main -c ./.config/polybar/config-main.ini &
+# polybar -q main_2 -c ./.config/polybar/config-main.ini &
+# polybar -q main_3 -c ./.config/polybar/config-main.ini &
+# polybar -q main_4 -c ./.config/polybar/config-main.ini &
+# polybar -q main_5 -c ./.config/polybar/config-main.ini &
+# polybar -q secondary -c ./.config/polybar/config-secondary.ini &
+# polybar -q secondary_2 -c ./.config/polybar/config-secondary.ini &
+# polybar -q secondary_3 -c ./.config/polybar/config-secondary.ini &
+# polybar -q secondary_4 -c ./.config/polybar/config-secondary.ini &
+# polybar -q secondary_5 -c ./.config/polybar/config-secondary.ini &
